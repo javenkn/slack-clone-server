55919fe597336b194147b2ed76ee127e54dc36c1
javenkn <javenkn7@yahoo.com>
2019-05-12 11:22:44 +0900

    syncs up the models using sequelize

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
+
 import typeDefs from './typeDefs';
 import resolvers from './resolvers';
+import models from './models';
 
 const server = new ApolloServer({
   typeDefs,
@@ -11,6 +13,12 @@ const app = express();
 
 server.applyMiddleware({ app });
 
-app.listen({ port: 3000 }, () =>
-  console.log(`🚀 Server ready at http://localhost:3000${server.graphqlPath}`),
-);
+models.sequelize
+  .sync({ force: true }) // { force: true } => drops tables before syncing
+  .then(() =>
+    app.listen({ port: 3000 }, () =>
+      console.log(
+        `🚀 Server ready at http://localhost:3000${server.graphqlPath}`,
+      ),
+    ),
+  );
