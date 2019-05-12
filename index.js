948211c681594403196c1dd16109be67dfd088bf
javenkn <javenkn7@yahoo.com>
2019-05-12 14:51:33 +0900

    installs merge-graphql-schemas package to split up graphql schemas

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
+import path from 'path';
+import { fileLoader, mergeTypes, mergeResolvers } from 'merge-graphql-schemas';
 
-import typeDefs from './typeDefs';
-import resolvers from './resolvers';
 import models from './models';
+
+const typeDefs = mergeTypes(fileLoader(path.join(__dirname, './schemas')));
+const resolvers = mergeResolvers(
+  fileLoader(path.join(__dirname, './resolvers')),
+);
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: {
+    models,
+    user: {
+      id: 1,
+    },
+  },
 });
 const app = express();
 
 server.applyMiddleware({ app });
 
 models.sequelize
-  .sync({ force: true }) // { force: true } => drops tables before syncing
+  .sync() // { force: true } => drops tables before syncing
   .then(() =>
     app.listen({ port: 3000 }, () =>
       console.log(
