529c8db12986c26a21113023732f096ea0323b86
javenkn <javenkn7@yahoo.com>
2019-05-12 14:50:37 +0900

    splits up schema types into their own files

---
+++ schemas/channel.js
@@ -0,0 +1,13 @@
+export default `
+  type Channel {
+    id: Int!
+    name: String!
+    public: Boolean!
+    messages: [Message!]!
+    users: [User!]!
+  }
+
+  type Mutation {
+    createChannel(name: String!, teamId: Int!, public: Boolean=false): Boolean!
+  }
+`;
