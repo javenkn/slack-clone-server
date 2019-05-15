7f7ce96d11d0706c90b79489880d731e6b62deff
javenkn <javenkn7@yahoo.com>
2019-05-15 20:01:19 +0900

    refactoring of the validation of the password and authentication

--- resolvers/user.js
+++ resolvers/user.js
@@ -1,4 +1,3 @@
-import bcrypt from 'bcrypt';
 import _ from 'lodash';
 
 import { tryLogin } from '../auth';
@@ -24,25 +23,9 @@ export default {
   Mutation: {
     login: (parent, { email, password }, { models, SECRET, SECRET2 }) =>
       tryLogin(email, password, models, SECRET, SECRET2),
-    register: async (parent, { password, ...other }, { models }) => {
+    register: async (parent, args, { models }) => {
       try {
-        if (password.length < 5 || password.length > 100) {
-          return {
-            ok: false,
-            errors: [
-              {
-                path: 'password',
-                message:
-                  'The password needs to be between 5 and 100 characters long.',
-              },
-            ],
-          };
-        }
-        const hashedPassword = await bcrypt.hash(password, 12);
-        const user = await models.User.create({
-          ...other,
-          password: hashedPassword,
-        });
+        const user = await models.User.create(args);
         return {
           ok: true,
           user,
