e135512f976aa26e7436370263b5a7e21ad915ff
javenkn <javenkn7@yahoo.com>
2019-05-25 17:06:02 +0900

    Fixes up the query for getting messages and authenticates it

--- src/resolvers/message.js
+++ src/resolvers/message.js
@@ -1,18 +1,34 @@
+import { combineResolvers } from 'graphql-resolvers';
+
+import { isAuthenticated } from '../helpers/permissions';
+
 export default {
   Query: {
-    messages: async (parent, args, { models, user }) => {
-      return [];
-    },
+    messages: combineResolvers(
+      isAuthenticated,
+      (parent, { channelId }, { models, user }) =>
+        models.Message.findAll(
+          { order: [['createdAt', 'ASC']], where: { channelId } },
+          { raw: true },
+        ),
+    ),
   },
   Mutation: {
-    createMessage: async (parent, args, { models, user }) => {
-      try {
-        await models.Message.create({ ...args, userId: user.id });
-        return true;
-      } catch (error) {
-        console.log(error);
-        return false;
-      }
-    },
+    createMessage: combineResolvers(
+      isAuthenticated,
+      async (parent, args, { models, user }) => {
+        try {
+          await models.Message.create({ ...args, userId: user.id });
+          return true;
+        } catch (error) {
+          console.log(error);
+          return false;
+        }
+      },
+    ),
+  },
+  Message: {
+    user: ({ userId }, args, { models }) =>
+      models.User.findOne({ where: { id: userId } }),
   },
 };
