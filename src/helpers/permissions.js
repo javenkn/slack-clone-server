b7332083fcdf0003e022e267e661b9040e7e27d2
javenkn <javenkn7@yahoo.com>
2019-05-26 15:45:39 +0900

    fixes subscriptions

--- src/helpers/permissions.js
+++ src/helpers/permissions.js
@@ -1,5 +1,22 @@
 import { ForbiddenError } from 'apollo-server-express';
-import { skip } from 'graphql-resolvers';
+import { combineResolvers, skip } from 'graphql-resolvers';
 
-export const isAuthenticated = (parent, args, { user }) =>
-  user ? skip : new ForbiddenError('Not authenticated as user.');
+export const isAuthenticated = (parent, args, { user }) => {
+  return user ? skip : new ForbiddenError('Not authenticated as user.');
+};
+
+export const isTeamMember = combineResolvers(
+  isAuthenticated,
+  (parent, { channelId }, { user, models }) => console.log('hello'),
+  // check if user is part of the team
+  // const channel = await models.Channel.findOne({
+  //   where: { id: channelId },
+  // });
+  // const member = await models.Member.findOne({
+  //   where: { teamId: channel.teamId, userId: user.id },
+  // });
+  // if (!member) {
+  //   throw new ForbiddenError('You have to be a member to subscribe.');
+  // }
+  // return skip;
+);
